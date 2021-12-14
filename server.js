b48bd540b230d190be60ec9bf243e445fcddec16
Donald Biggers <DuGsEmail@yahoo.com>
2021-12-14 10:44:42 -0600

    tryagain

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const { animals } = require('./data/animals');
 const express = require('express');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -42,6 +41,9 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
+
+  const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
