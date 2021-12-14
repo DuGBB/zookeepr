442c97a53a1cfea034f1a62938db96dcd1152634
Donald Biggers <DuGsEmail@yahoo.com>
2021-12-14 11:48:26 -0600

    handles requests for specific searches

--- server.js
+++ server.js
@@ -43,7 +43,12 @@ function filterByQuery(query, animalsArray) {
   }
 
   const PORT = process.env.PORT || 3001;
-const app = express();
+  const app = express();
+
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -51,6 +56,15 @@ app.get('/api/animals', (req, res) => {
       results = filterByQuery(req.query, results);
     }
     res.json(results);
+  });
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+      if (result) {
+        res.json(result);
+      } else {
+        res.send(404);
+      }
   });
 
 app.listen(PORT, () => {
